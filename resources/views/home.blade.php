22ab6f9a1ea0511de4496bfa97bc20d630da3332
AlanCruz08 <alansasuke0@gmail.com>
2024-10-03 13:56:17 -0600

    Creacion migracion mensajes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+<div class="container mt-5">
+    <div class="row">
+        <!-- Columna de Contactos -->
+        <div class="col-md-4">
+            <div class="list-group">
+                @foreach ($contacts as $contact)
+                    <a href="{{ route('contacts.index', ['contactId' => $contact->id]) }}" 
+                       class="list-group-item list-group-item-action {{ $selectedContact && $selectedContact->id == $contact->id ? 'active' : '' }}">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-1">{{ $contact->name }}</h5>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                        <p class="mb-1 text-muted">
+                            Last message: {{ $contact->messages->last()->content ?? 'No messages yet' }}
+                        </p>
+                    </a>
+                @endforeach
             </div>
+        </div>
+
+        <!-- Columna de Mensajes -->
+        <div class="col-md-8">
+            @if ($selectedContact)
+                <h3>Messages with {{ $selectedContact->name }}</h3>
+                <ul class="list-group">
+                    @foreach ($messages as $message)
+                        <li class="list-group-item">{{ $message->content }}</li>
+                    @endforeach
+                </ul>
+            @else
+                <div class="alert alert-info" role="alert">
+                    Select a contact to view messages
+                </div>
+            @endif
         </div>
     </div>
 </div>
